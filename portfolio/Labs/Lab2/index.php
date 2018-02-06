ed9133cab9e24a7efa0334eae1be39470e9c59ad
Ruben Vasquez <rubvasquez@csumb.edu>
2018-02-06 23:22:19 +0000

    Lab 1

--- portfolio/Labs/Lab2/index.php
+++ portfolio/Labs/Lab2/index.php
@@ -2,8 +2,55 @@
 <html>
     <head>
         <title> 777 Slot Machine </title>
+        <meta charset="utf-8" />
     </head>
     <body>
+        
+        <?php
+        
+        function displaySymbol($randomValue){
+        $randomValue = rand(0,2);
+        echo $randomValue;
+        
+        // if ($randomValue == 0){
+        //     $symbol = "seven";
+        // }   else if($randomValue == 1) {
+        //     $symbol = "orange";
+        // }  else {
+        //     $symbol = "cherry";
+        // }
+        
+        switch ($randomValue):
+            case 0: $symbol ="seven";
+                 break;
+            case 1: $symbol = "orange";
+                 break;
+             case 2: $symbol = "cherry";
+                  break;
+            
+        echo "<img src='img/$symbol.png' width='70' alt='$symbol' title='$symbol' />";
+        
+        }
+        
+        $randomValue1 = rand(0,2);
+        displaySymbol($randomValue1);
+         $randomValue2 = rand(0,2);
+        displaySymbol($randomValue2);
+         $randomValue3 = rand(0,2);
+        displaySymbol($randomValue3);
+        
+        echo "<br /> <hr /> values: $randomValue1 $randomValue2 $randomValue3 ";
+        
+        // for($i=0;$i<3;$i++){
+          //   displaySymbol();
+        // }
+        
+        
+        ?>
+        
+     <!--   <img src="img/lemon.png" width="70" alt="Lemon" title="Lemon" />
+        <img src="img/cherry.png" width="70" alt="Cherry" title="Cherry" />
+        <img src="img/orange.png" width="70" alt="Orange" title="Orange" /> -->
 
     </body>
 </html>
