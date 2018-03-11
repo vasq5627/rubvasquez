8790af9c423c1be4fabaf843d3626b6ac3b4c75f
Ruben Vasquez <rubvasquez@csumb.edu>
2018-03-11 08:25:32 +0000

    Lab 4

--- Labs/Lab4/index.php
+++ Labs/Lab4/index.php
@@ -1,29 +1,47 @@
 <?php
+
  //print_r($_GET); //displaying all content submitted in the form using the GET method
+
   $backgroundImage = "img/sea.jpg";
-    if (isset($_GET['keyword'])) { //if form was submitted
+    
+  if (isset($_GET['keyword'])) { //if form was submitted
+      
       include 'api/pixabayAPI.php';
+      
       echo "<h3>You searched for " . $_GET['keyword'] . "</h3>";
+      
       $orientation = "horizontal";
       $keyword = $_GET['keyword'];
-      if (isset($_GET['layout'])) 
-        {  //user checked a layout
+      
+      if (isset($_GET['layout'])) {  //user checked a layout
+        
         $orientation = $_GET['layout'];
+        
       }
+      
       if (!empty($_GET['category'])) { //user selected a category
         $keyword = $_GET['category'];
       }
+      
       $imageURLs = getImageURLs($keyword, $orientation);
+      
       //$backgroundImage = $imageURLs[rand(0, count($imageURLs)-1];
       $backgroundImage = $imageURLs[array_rand($imageURLs)];
+      
       //print_r($imageURLs);
+      
+      
   }      
-  function checkCategory($category){
-    if($category == $_GET['Category']){
-      echo "selected";
+ 
+ function checkCategory($category){
+   
+    if ($category == $_GET['category']) {
+       echo " selected";
     }
-  }
+ }
+
 ?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -35,35 +53,56 @@
         body {
             background-image: url(<?=$backgroundImage?>);
         }
+        
         #carouselExampleIndicators {
             width: 500px;
-            margin: 0 auto; //centers a div container
+            margin: 0 auto; /*centers a div container*/
         }
+         
     </style>
     <body>
+
         <?php
+        
             if (!isset($_GET['keyword'])) {
+        
               echo "<h2> You must type a keyword or select a category </h2>";
+            
             }  
         ?>
+
+        
+
         <form method="GET">
-            <input type="text" size="20" name="keyword" placeholder="Keyword to search for"  value="<?=$_GET['keyword']?>"/>
-            <input type="radio" name="layout" value="horizontal" id="hlayout">
-            <?php 
-              if ($_GET['layout'] == "horzontal"){
-                echo "checked";
-              }
+            
+            <input type="text" name="keyword" placeholder="Keyword to search for" value="<?=$_GET['keyword']?>"/>
+            
+            <input type="radio" name="layout" value="horizontal" id="hlayout"> 
+            
+            <?php
+               if ($_GET['layout'] == "horizontal") {
+                 echo "checked";
+               }
+                
             ?>
-            <label for="hlayout">Horizontal</label>
+            
+            />
+            <label for="hlayout"></label>Horizontal </label>
+            
             <input type="radio" name="layout" value="vertical" id="vlayout" <?= ($_GET['layout']=="vertical")?"checked":"" ?>>
-            <label for="vlayout"> vertical </label> 
-             <select name="category">
+            <label for="vertical">Vertical </label>
+            
+            <select name="category">
               <option value="" >  Select One </option> 
               <option value="sea" <?=checkCategory('sea')?>>  Ocean </option>
-              <option selected <?=checkCategory('forest')?>>  Forest </option>
-              <option <?=checkCategory('sky')?>>  Sky </option>
+              <option value="forest" <?=checkCategory('Forest')?>>  Forest </option>
+              <option value="sky"<?=checkCategory('Sky')?>>  Sky </option>
+               <option value="city" <?=checkCategory('City')?>>  City </option>
+               <option value="snow" <?=checkCategory('Snow')?>>  Snow </option>
+               
             </select>
-           
+            
+            
             <input type="submit" value="Submit!"/>
                     
         </form>
@@ -76,8 +115,15 @@
         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
               <ol class="carousel-indicators">
                 <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <?php for ($i=1; $i < 7; $i++) { ?>
+                  <li data-target="#carouselExampleIndicators" data-slide-to="<?=$i?>"></li>
+                <?php } //endFor ?> 
+                <!--
                 <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+                -->
               </ol>
               <div class="carousel-inner">
                 <div class="carousel-item active">
@@ -88,6 +134,18 @@
                 </div>
                 <div class="carousel-item">
                   <img class="d-block w-100" src="<?=$imageURLs[2]?>" alt="Third slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="<?=$imageURLs[3]?>" alt="Fourth slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="<?=$imageURLs[4]?>" alt="Fifth slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="<?=$imageURLs[5]?>" alt="Sixth slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="<?=$imageURLs[6]?>" alt="Seven slide">
                 </div>
               </div>
               <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -102,6 +160,7 @@
         
         <?php
             }//endIf
+           
         ?>
         
         
