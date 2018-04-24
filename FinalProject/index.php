ae901d4f89594c0696d6d6aaba5aa65183ddc37f
Ruben Vasquez <rubvasquez@csumb.edu>
2018-04-24 06:13:18 +0000

    4/23/18

--- FinalProject/index.php
+++ FinalProject/index.php
@@ -2,9 +2,9 @@
 <html>
     <head>
         <style>
-             @import url("css/styles.css");
+             @import url("css/style.css");
         </style>
-        <title>  </title>
+        <title> Outdoor Clothing Store </title>
          <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
@@ -12,6 +12,57 @@
     </head>
     <body>
         
+        <form>
+             <h1>  Outdoor Clothing Store  </h1>
+            Product: <input type="text" name="objectName" /><br /><br />
+            
+            Piece: 
+                <select name="piece">
+                    <option value=""> Select One </option>
+        
+                </select>
+            <br /><br />
+            
+            Brand: 
+                <select name="brand">
+                    <option value=""> Select One </option>
+                    
+                </select>
+            <br /><br />
+            
+            Price:  From <input type="text" name="priceFrom" size="7"/>
+                    To   <input type="text" name="priceTo" size="7"/>
+                    
+            <br /><br />
+            
+             Order result by:<br />
+             
+             <input type="radio" name="orderBy" value="price"/> Price 
+             
+             <br /><br />
+             
+             <input type="radio" name="orderBy" value="name"/> Name
+             
+             <br /><br />
+             <div class="btn-group btn-group-toggle" data-toggle="buttons">
+  <label class="btn btn-primary">
+    <input type="radio" name="orderBy" value="price" autocomplete="off"> Price
+  </label>
+  <label class="btn btn-success">
+    <input type="radio" name="orderBy"  value="name"  autocomplete="off"> Name
+  </label>
+</div>
+             <br /><br />
+             <input type="submit" value="Search" name="searchForm" />
+             
+        </form>
+        
+        <br />
+        <hr>
+        <form>
+    
+        </form>
+
 
     </body>
 </html>
--- Labs/Lab8/hangman/js/hangman.js
+++ Labs/Lab8/hangman/js/hangman.js
@@ -1,53 +1,181 @@
-  
-            var selectedWord = "";
-            var selectedHint = "";
-            var board = []; //array
-            var remainingGuesses = 6;
-            var words = ["snake", "monkey", "bettle", "horse", "dog"];
-            var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 
-                'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 
-                'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-            
-            
-            //console.log(words[0]);
-            
-            window.onload = startGame();
-            //$("#letterBtn").click( function(){ 
-              //  alert($("#letterBox").val() ); } );
-            //document.getElementById("letterBtn")
-            $(".letter").click(function(){
-                console.log($(this).attr("id"));
-            });
-            
-            function startGame(){
-                pickWord();
-                initboard();
-                updateBoard();
-            }
-            
-            function pickWord(){
-            var randomInt = Math.floor(Math.random() * words.length);
-            selectedWord = words[randomInt];
-            }
-            
-            function createLetters(){
-                for (var letter of alphabet){
-                    $("#letters").append("<button class='letter' id='" + letter + "'>" + letter + "</button>"); 
-                }
-            }
-            
-            //for (var i=0; i < selectedWord.length; i++){
-              //  board.push("_");
-            //}
-            function initboard(){
-            for(var letter in selectedWord){
-                board.push("_");
-            }
-            }
-            
-          
-          function updateBoard(){
-            for(var letter of board){
-                 document.getElementById("word").innerHTML += letter + " ";
-            }
-          }
+
+var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 
+                'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 
+                'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+                
+var selectedWord = "";
+var selectedHint = "";
+var remainingGuesses =  6;
+var board = []; // array
+// var board = new Array;
+var words= [{word: "snake", hint: "It's a reptile!"},
+            {word: "monkey", hint: "It's a primate!"},
+            {word: "beetle", hint: "It's an insect!"},
+            {word: "horse", hint: "It's a trusty steady!"},
+            {word: "dog", hint: "It's a man's best friend!"}];
+
+
+window.onload = startGame();
+
+// $("#letterBtn").click(function()
+// { 
+   
+//      alert($("#letterBox").val()); 
+     
+// });
+ 
+$(".letter").click( function() 
+{ 
+    checkLetter($(this).attr("id")); 
+    disableButton($(this));
+});
+ 
+$(".replayBtn").on("click", function()
+{
+    location.reload();
+});
+$(".hint").on("click", function()
+{
+    $("#word").append("<span class='hint'>Hint: " + selectedHint + "</span>");
+    $("#hint").hide();
+    remainingGuesses -= 1;
+    updateMan();
+});
+
+
+ 
+ //document.getElementById("letterBtn")
+ 
+ 
+
+function startGame()
+{ 
+    pickWord();
+    initBoard();
+    createLetters();
+    updateBoard();
+}
+
+// alert(words[[0]);
+// console.log(words[5]);
+function pickWord()
+{
+    var randomInt = Math.floor(Math.random() * words.length);
+    console.log(randomInt);
+    selectedWord = words[randomInt].word.toUpperCase();
+    selectedHint = words[randomInt].hint;
+    // console.log(selectedWord);
+}
+
+
+
+// for(var i=0; i < selectedWord.length; i++)
+// {
+//     board.push("_");
+// }
+
+function initBoard()
+{
+    for(var letters in selectedWord)
+    {
+        board.push("_");
+    }
+    
+    console.log(board);
+}
+
+function createLetters()
+{
+    for(var letter of alphabet)
+    {
+        $("#letters").append("<button class='letter' id='" + letter + "'>" + letter + "</button>");
+    }
+}
+
+function checkLetter(letter)
+{
+    var positions = new Array();
+    
+    for (var i = 0; i < selectedWord.length; i++)
+    {
+        console.log(selectedWord)
+        if(letter == selectedWord[i])
+        {
+            positions.push(i);
+        }
+    }
+    
+    if(positions.length > 0)
+    {
+        updateWord(positions, letter);
+        
+        if(!board.includes('_'))
+        {
+            endGame(true);
+            $("#guesses").append("<h5>" + selectedWord + "</h5><br/>");
+        }
+    }
+    else
+    {
+        remainingGuesses -= 1;
+        updateMan();
+        $("#hint").show();
+    }
+    if(remainingGuesses <= 0)
+    {
+        endGame(false);
+        $("#guesses").append("<h5>" + selectedWord + "</h5><br/>");
+    }
+}
+
+function updateWord(positions, letter)
+{
+    for(var pos of positions)
+    {
+        board[pos] = letter;
+    }
+    
+    updateBoard();
+}
+
+function updateBoard()
+{
+    $("#word").empty();
+    
+    // for (var letter of board) 
+    // {
+    //     document.getElementById("word").innerHTML += letter + " ";
+    // }
+    
+    for(var i=0; i < board.length; i++)
+    {
+        $("#word").append(board[i] + " ")
+    }
+    $("#word").append("<br />")
+}
+
+function updateMan()
+{
+    $("#hangImg").attr("src", "img/stick_" + (6 - remainingGuesses) + ".png");
+}
+
+
+function disableButton(btn)
+{
+    btn.prop("disabled", true);
+    btn.attr("class", "btn btn-danger");
+}
+
+function endGame(win)
+{
+    $("#letters").hide();
+    
+    if(win)
+    {
+        $("#won").show();
+    }
+    else
+    {
+        $("#lost").show();
+    }
+}
